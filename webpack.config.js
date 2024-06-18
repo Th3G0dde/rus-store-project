2e37e924a6c9629695b531c638ff7be67829877a
Dmitriy <TheGoddes@yandex.ru>
2024-06-18 15:27:04 +0300

    one

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
-// webpack.config.js
-const path = require('path'); // подключаем path к конфигу вебпак
-
+// Webpack v4
+const path = require('path');
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-                publicPath: ''
-    }
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
 }
-
-// переписали точку выхода, используя утилиту path
